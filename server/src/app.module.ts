7faeb7fd29d8341cdb3e440714dc217216a69f05
Anmol Rai <anmolrai0307@gmail.com>
2026-09-16 23:16:03 +0530

    chore: prepare frontend for deployment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,31 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsModule } from './jobs/jobs.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'better-sqlite3',
-      database: 'database.sqlite',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(
+      isProduction
+        ? {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: true,
+          }
+        : {
+            type: 'better-sqlite3',
+            database: 'database.sqlite',
+            autoLoadEntities: true,
+            synchronize: true,
+          },
+    ),
     JobsModule,
   ],
   controllers: [AppController],
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,15 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+  });
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
-
 }
 
 bootstrap();
